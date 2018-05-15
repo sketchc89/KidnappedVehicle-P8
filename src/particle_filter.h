72f65c433dba6ea74a88388263c3a90990092b1b
Chris Sketch <sketchc89@gmail.com>
2018-05-15 14:04:03 -0400

    Rename class members and classes

--- src/particle_filter.h
+++ src/particle_filter.h
@@ -28,15 +28,15 @@ struct Particle {
 class ParticleFilter {
 	
 	// Number of particles to draw
-	int num_particles; 
+	int num_particles_; 
 	
 	
 	
 	// Flag, if filter is initialized
-	bool is_initialized;
+	bool is_initialized_;
 	
-	// Vector of weights of all particles
-	std::vector<double> weights;
+	// Vector of weights_ of all particles
+	std::vector<double> weights_;
 	
 public:
 	
@@ -44,14 +44,14 @@ public:
 	std::vector<Particle> particles;
 
 	// Constructor
-	// @param num_particles Number of particles
-	ParticleFilter() : num_particles(0), is_initialized(false) {}
+	// @param num_particles_ Number of particles
+	ParticleFilter() : num_particles_(0), is_initialized_(false) {}
 
 	// Destructor
 	~ParticleFilter() {}
 
 	/**
-	 * init Initializes particle filter by initializing particles to Gaussian
+	 * Init Initializes particle filter by initializing particles to Gaussian
 	 *   distribution around first position and all the weights to 1.
 	 * @param x Initial x position [m] (simulated estimate from GPS)
 	 * @param y Initial y position [m]
@@ -59,10 +59,10 @@ public:
 	 * @param std[] Array of dimension 3 [standard deviation of x [m], standard deviation of y [m]
 	 *   standard deviation of yaw [rad]]
 	 */
-	void init(double x, double y, double theta, double std[]);
+	void Init(double x, double y, double theta, double std[]);
 
 	/**
-	 * prediction Predicts the state for the next time step
+	 * Prediction Predicts the state for the next time step
 	 *   using the process model.
 	 * @param delta_t Time between time step t and t+1 in measurements [s]
 	 * @param std_pos[] Array of dimension 3 [standard deviation of x [m], standard deviation of y [m]
@@ -70,32 +70,32 @@ public:
 	 * @param velocity Velocity of car from t to t+1 [m/s]
 	 * @param yaw_rate Yaw rate of car from t to t+1 [rad/s]
 	 */
-	void prediction(double delta_t, double std_pos[], double velocity, double yaw_rate);
+	void Prediction(double delta_t, double std_pos[], double velocity, double yaw_rate);
 	
 	/**
-	 * dataAssociation Finds which observations correspond to which landmarks (likely by using
+	 * DataAssociation Finds which observations correspond to which landmarks (likely by using
 	 *   a nearest-neighbors data association).
 	 * @param predicted Vector of predicted landmark observations
 	 * @param observations Vector of landmark observations
 	 */
-	void dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations);
+	void DataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations);
 	
 	/**
-	 * updateWeights Updates the weights for each particle based on the likelihood of the 
+	 * UpdateWeights Updates the weights for each particle based on the likelihood of the 
 	 *   observed measurements. 
 	 * @param sensor_range Range [m] of sensor
 	 * @param std_landmark[] Array of dimension 2 [Landmark measurement uncertainty [x [m], y [m]]]
 	 * @param observations Vector of landmark observations
 	 * @param map Map class containing map landmarks
 	 */
-	void updateWeights(double sensor_range, double std_landmark[], const std::vector<LandmarkObs> &observations,
+	void UpdateWeights(double sensor_range, double std_landmark[], const std::vector<LandmarkObs> &observations,
 			const Map &map_landmarks);
 	
 	/**
-	 * resample Resamples from the updated set of particles to form
+	 * Resample Resamples from the updated set of particles to form
 	 *   the new set of particles.
 	 */
-	void resample();
+	void Resample();
 
 	/*
 	 * Set a particles list of associations, along with the associations calculated world x,y coordinates
@@ -105,15 +105,15 @@ public:
 		                     const std::vector<double>& sense_x, const std::vector<double>& sense_y);
 
 	
-	std::string getAssociations(Particle best);
-	std::string getSenseX(Particle best);
-	std::string getSenseY(Particle best);
+	std::string GetAssociations(Particle best);
+	std::string GetSenseX(Particle best);
+	std::string GetSenseY(Particle best);
 
 	/**
 	* initialized Returns whether particle filter is initialized yet or not.
 	*/
-	const bool initialized() const {
-		return is_initialized;
+	const bool Initialized() const {
+		return is_initialized_;
 	}
 };
 
