7db4ca57ad101bfc1a24b47ec4afc5ae68611572
Chris Sketch <sketchc89@gmail.com>
2018-05-18 18:40:00 -0400

    Catch testing

---
+++ src/tests/test_x.cpp
@@ -0,0 +1,11 @@
+#ifndef M_PI
+  #define M_PI 3.1415926535897;
+#endif
+const double M_EPS = 0.000001;
+#include "catch.hpp"
+#include "../file_to_test.cpp"
+
+TEST_CASE("", "[]") {
+  SECTION("") {
+    REQUIRE();
+}
