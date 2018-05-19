a5b5bcca8b32cfc290e34af730a6da0da2d4011b
Chris Sketch <sketchc89@gmail.com>
2018-05-19 10:38:18 -0400

    Change script to account for linking errors catch2

--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -7,8 +7,8 @@ for filename in './'*.cpp; do
   if [ $cppfile == "tests_main.cpp" ]; then
     continue
   fi
-  echo -e "\nCompiling $filename"
-  g++ --std=c++11 tests_main.o $cppfile -o $execfile
+  echo -e "\nCompiling $filename to $execfile"
+  g++ --std=c++11 tests_main.cpp $cppfile -o $execfile
   echo -e "\nRunning $execfile"
   $fpath/$execfile
 done
