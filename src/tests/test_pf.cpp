8ebcac4492e96287843bbde1e69859c81a32d615
Chris Sketch <sketchc89@gmail.com>
2018-05-19 12:17:09 -0400

    Testing: ParticleFilter::Prediction, yaw rate = 0

--- src/tests/test_pf.cpp
+++ src/tests/test_pf.cpp
@@ -8,7 +8,6 @@ const double M_EPS = 0.000001;
 
 TEST_CASE("Particle filter", "[particle_filter]") {
   ParticleFilter pf;
-  Approx target = Approx(0.0).margin(0.3);
 
   double x=0, y=0, theta=0;
   double variance[3] = {0.3, 0.3, 0.01};
@@ -23,8 +22,36 @@ TEST_CASE("Particle filter", "[particle_filter]") {
       sum_y += particle->y;
       sum_theta += particle->theta;
     }
+    Approx target = Approx(0.0).margin(0.3);
     REQUIRE(sum_x/pf.particles.size() == target); 
     REQUIRE(sum_y/pf.particles.size() == target); 
-    REQUIRE(sum_theta/pf.particles.size() == target); 
+    REQUIRE(sum_theta/pf.particles.size() == target);
+  }
+  SECTION("Predict position after movement - yaw rate = 0", "[Prediction]") {
+    double dt = 0.1;
+    double sig_pos[3] = {0.3, 0.3, 0.01};
+    double v = 10;
+    double ydd=0.0;
+
+    std::vector<Particle> particles;
+    for (int i=0; i<100; ++i) {
+      Particle p;
+      p.x = 0.0;
+      p.y = 0.0;
+      p.theta = 0.0;
+      particles.push_back(p);
+    }
+    pf.particles = particles;
+    pf.Prediction(dt, sig_pos, v, ydd);
+    double sum_x = 0, sum_y = 0, sum_theta = 0;
+    for (auto particle=pf.particles.begin(); particle != pf.particles.end(); ++particle) {
+      sum_x += particle->x;
+      sum_y += particle->y;
+      sum_theta += particle->theta;
+    }
+    REQUIRE(sum_x/pf.particles.size() == Approx(1.0).margin(0.03)); 
+    REQUIRE(sum_y/pf.particles.size() == Approx(0.0).margin(0.03)); 
+    REQUIRE(sum_theta/pf.particles.size() == Approx(0.0).margin(0.001));
+
   }
 }
